a4c5c36fea38b4511f36bd9e88ae6d44b6208121
Luiz <luiz.ernica@gmail.com>
2026-06-19 17:55:28 -0300

    feat: First commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer =
+    new ProducerBuilder<Null, string>(config)
+    .Build();
+
+var random = new Random();
+
+while (true)
+{
+    double temperature =
+        Math.Round(random.NextDouble() * 40, 1);
+
+    string message =
+        $"Temperature={temperature}";
+
+    var result =
+        await producer.ProduceAsync(
+            "Temperature_topic",
+            new Message<Null, string>
+            {
+                Value = message
+            });
+
+    Console.WriteLine(
+        $"Sent: {message} | " +
+        $"Partition: {result.Partition} | " +
+        $"Offset: {result.Offset}");
+
+    await Task.Delay(2000);
+}
